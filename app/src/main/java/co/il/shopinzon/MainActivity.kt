81dd5aaf8b944e12c9042ffeb9b484ddd6db0145
yair <yairopro@gmail.com>
2026-01-07 23:37:42 +0200

    increment

--- app/src/main/java/co/il/shopinzon/MainActivity.kt
+++ app/src/main/java/co/il/shopinzon/MainActivity.kt
@@ -6,9 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.clickable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.graphics.Color
+import co.il.shopinzon.ui.theme.BrightBlue
+import co.il.shopinzon.ui.theme.SkyBlue
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import co.il.shopinzon.ui.theme.ShopinILTheme
@@ -20,9 +36,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShopinILTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    TapCounter(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
                     )
                 }
             }
@@ -31,17 +48,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun TapCounter(modifier: Modifier = Modifier) {
+    var tapCount by remember { mutableStateOf(0) }
+    
+    Box(
         modifier = modifier
-    )
+            .clickable { tapCount++ }
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "$tapCount",
+                fontSize = 80.sp,
+                color = if (MaterialTheme.colorScheme.background == Color.Black) BrightBlue else SkyBlue
+            )
+        }
+        
+        Text(
+            text = "tap to increment",
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TapCounterPreview() {
     ShopinILTheme {
-        Greeting("Android")
+        TapCounter()
     }
 }
